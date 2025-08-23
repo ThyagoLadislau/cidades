c29564e6a12e40b9702f42d1195342cfdbe2ebf7
Thyago <thyago2012ladislau@gmail.com>
2025-08-22 22:44:22 -0300

    concluida

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Bertioga");
+Console.WriteLine("Cudatão");
+Console.WriteLine("Guarujá");
+Console.WriteLine("Bertioga");
+Console.WriteLine("Bertioga");
+Console.WriteLine("Bertioga");
+Console.WriteLine("Bertioga");
